3e2b08b94cb1bb304b74260e8308f22b8b1cd7f6
MrArSavchuk <mrarsavchuk@gmail.com>
2025-09-06 02:55:04 -0400

    Add AD№17

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import BookingForm from "./components/BookingForm";
 import ManageBooking from "./components/ManageBooking";
 import PromoBillboard from "./components/PromoBillboard";
-import "./styles.css";
 
 export default function App() {
   const [theme, setTheme] = useState(() => localStorage.getItem("tt-theme") || "dark");
